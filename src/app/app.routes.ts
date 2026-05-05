754478f035ccfa72020d34a8267b4cc7503a564d
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2026-05-05 16:12:14 +0200

    introduce routing with home and add-attraction views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AddAttraction } from './pages/add-attraction/add-attraction';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+
+    { path: 'add-attraction', component: AddAttraction },
+
+    { path: 'attractions/:uuid', component: Home },
+
+    { path: '**', redirectTo: '' }
+
+];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -4,11 +4,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
